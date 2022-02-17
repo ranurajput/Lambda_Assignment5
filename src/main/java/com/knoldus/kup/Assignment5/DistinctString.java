e532cf462340c979582c49aaf543373979e4e835
ranurajput <rrajput9760@gmail.com>
2022-02-17 18:50:56 +0530

    Lambda Assignment 5

---
+++ src/main/java/com/knoldus/kup/Assignment5/DistinctString.java
@@ -0,0 +1,37 @@
+package com.knoldus.kup.Assignment5;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+public class DistinctString {
+    public static List<String> getDistinctStrings(List<String> stringList){
+        // writing lambda for the given requirement
+        Function<List<String>, List<String>> distinctStrings = str -> str.stream()
+                .distinct() // getting distinct elements
+                .sorted() // sorting the list
+                .collect(Collectors.toList()); // collecting as a list
+        return distinctStrings.apply(stringList);
+    }
+    public static void main(String[] args) {
+        List<String> list= new ArrayList<>();
+        System.out.println("Compiled by Ranu Rajput");
+        System.out.println("Type words in single line using space and press enter");
+        Scanner input =new Scanner(System.in);
+        String line = input.nextLine();
+
+        String[] stringList3= line.split("\\s");
+        for (String str:stringList3) {
+            list.add(str);
+        }
+//      output
+        System.out.println("Distinct Values"+DistinctString.getDistinctStrings(list));
+    }
+}
+/*
+OUTPUT-->
+Compiled by Ranu Rajput
+Type words in single line using space and press enter
+Ranu Rajput is a Java Developer
+Distinct Values[Developer, Java, Rajput, Ranu, a, is]
+ */
---
+++ src/test/java/com/knoldus/kup/Assignment5/DistinctStringTests.java
@@ -0,0 +1,43 @@
+package com.knoldus.kup.Assignment5;
+
+import junit.framework.Assert;
+//import static org.junit.Assert;
+//import org.junit.Test;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DistinctStringTests {
+
+    //      Given test cases
+    List<String> stringList = new ArrayList<>(Arrays.asList("java", "scala", "java", "clojure", "clojure"));
+    List<String> stringList2 = new ArrayList<>(Arrays.asList("the", "three", "the", "three", "the", "three","an","an","a"));
+
+    @Test
+    public void shouldReturnDistinct(){
+        List<String> expected1 = Arrays.asList("clojure","java","scala");
+        List<String> expected2 = Arrays.asList("a","an","the","three");
+
+        List<String> distincList1= DistinctString.getDistinctStrings(stringList);
+        List<String> distincList2= DistinctString.getDistinctStrings(stringList2);
+
+        Assert.assertEquals(expected1,distincList1);
+        Assert.assertEquals(expected2,distincList2);
+        System.out.println("Test case has passed Successfully");
+        System.out.println(expected1);
+        System.out.println(expected2);
+    }
+}
+/*
+OUTPUT-->
+Test case has passed Successfully
+[clojure, java, scala]
+[a, an, the, three]
+===============================================
+Default Suite
+Total tests run: 1, Passes: 1, Failures: 0, Skips: 0
+===============================================
+
+ */
